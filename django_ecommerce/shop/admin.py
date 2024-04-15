3ab5f1793896d700773f11e45d123d5a9e625e41
Karushkin Gleb <onlythreebythree@gmail.com>
2024-04-15 19:33:26 +0400

    Category and product models registration

--- django_ecommerce/shop/admin.py
+++ django_ecommerce/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    prepopulated_fields = {'slug': ['name']}
+    
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category', 'name', 'price', 'available', 'created']
+    list_filter = ['available', 'created']
+    prepopulated_fields = {'slug': ['name']}
